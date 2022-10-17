369257e4596117a41bb94fc61899b0c43384366a
moddas311 <moddasir311@gmail.com>
2022-10-17 19:58:15 +0600

    created private route and updeted it

---
+++ src/farebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVhCzdEe0-YC92Wm7aQcJGbjzxY0RmP4w",
+  authDomain: "ema-john-simple-b3c3f.firebaseapp.com",
+  projectId: "ema-john-simple-b3c3f",
+  storageBucket: "ema-john-simple-b3c3f.appspot.com",
+  messagingSenderId: "699838181529",
+  appId: "1:699838181529:web:8bee536f50115371314808"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
